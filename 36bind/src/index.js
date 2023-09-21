3720e20271af1f873758c41a7a47014f51178e91
vivien_yang <1740768148@qq.com>
2023-09-21 18:51:51 +0800

    fix: 手写bind-支持new

--- 36bind/src/index.js
+++ 36bind/src/index.js
@@ -6,17 +6,36 @@ function bind(asThis){
   if(typeof fn !== 'function'){
     throw new Error('bind必须调用在函数身上')
   }
-  return function(){
+  function resultFn(){
     var args2=slice.call(arguments,0) // 不可以省略，伪数组转成数组
-    return fn.apply(asThis,args1.concat(args2))
+    return fn.apply(resultFn.prototype.isPrototypeOf(this)?this:asThis,
+      args1.concat(args2))
   }
+  resultFn.prototype=fn.prototype
+  return resultFn
 }
 // 简洁版-使用了es6的语法，（但是bind都不支持，es6的新语法也不支持）
 function _bind(asThis, ...args){
   let fn = this
-  return function(...args2){
-    return fn.call(asThis,...args,...args2)
+  function resultFn(...args2){
+    /**
+     * new 做了什么
+     * this; //new 生成的temp也就是this
+     * this.__proto__=resultFn.prototype
+     * this.p1='x1'; this.p2='y1'
+     * return this
+     */
+    // resultFn.prototype.isPrototypeOf(this)
+    // this instanceof resultFn 
+    return fn.call(this instanceof resultFn ? this : asThis,
+      ...args,
+      ...args2
+    )
   }
+  // console.log('fn.prototype',fn.prototype)
+  // console.log('resultFn.prototype',resultFn.prototype)
+  resultFn.prototype = fn.prototype
+  return resultFn
 }
 // export default bind
 module.exports = bind
--- 36bind/test/index.js
+++ 36bind/test/index.js
@@ -5,6 +5,8 @@ test2('测试fn.bind(asThis)')
 test3('测试fn.bind(asThis, param1, param2)')
 test4('测试 fn.bind(asThis)(param1, param2)')
 test5('测试 fn.bind(asThis, param1, param2)(p3, p4)')
+test6('new 的时候绑定了p1, p2')
+test7('new 的时候绑定了p1, p2,并且fn有prototype.sayHi')
 function test1(message){
   console.log(message)
   Function.prototype.bind2=bind
@@ -50,12 +52,47 @@ function test5(message){
   console.log(message)
   Function.prototype.bind2=bind
   // 测试 fn.bind(asThis, param1, param2)(p3, p4)
-  const fn2 = function(p1,p2){
-    return [this,p1,p2]
+  const fn2 = function(p1, p2, p3, p4){
+    return [this, p1, p2, p3, p4]
   }
-  const otherFn2 = fn2.bind2({aaa:'bbb'},666)
-  console.assert(otherFn2('s1')[0].aaa === 'bbb')
-  console.assert(otherFn2('s1')[1] === 666)
-  console.assert(otherFn2('s1')[2] === 's1')
+  const otherFn2 = fn2.bind2({aaa:'bbb'}, 666, 777)
+  console.assert(otherFn2('s1','s2')[0].aaa === 'bbb')
+  console.assert(otherFn2('s1','s2')[1] === 666)
+  console.assert(otherFn2('s1','s2')[2] === 777)
+  console.assert(otherFn2('s1','s2')[3] === 's1')
+  console.assert(otherFn2('s1','s2')[4] === 's2')
 }
 
+function test6(message){
+  console.log(message)
+  Function.prototype.bind2=bind
+  // new 的时候绑定了p1, p2
+  const fn = function(p1,p2){
+    this.p1 = p1
+    this.p2 = p2
+  }
+  const fn2 = fn.bind2(undefined,'x1','y1')
+  const obj = new fn2();
+  console.log(obj)
+  console.assert(obj.p1 === 'x1')
+  console.assert(obj.p2==='y1')
+}
+
+function test7(message){
+  console.log(message)
+  Function.prototype.bind2=bind
+  // new 的时候绑定了p1, p2,并且fn有prototype.sayH
+  const fn = function(p1,p2){
+    this.p1 = p1
+    this.p2 = p2
+  }
+  fn.prototype.sayHi=function(){return 'hi'}
+  const fn2 = fn.bind2(undefined,'x1','y1')
+  const obj = new fn2();
+  console.assert(obj.p1 === 'x1')
+  console.assert(obj.p2==='y1')
+  // console.log('obj.sayHi',obj.sayHi)
+  console.assert(obj.__proto__ === fn.prototype)
+  console.assert(typeof obj.sayHi === 'function')
+  console.assert(obj.sayHi() === 'hi')
+}
